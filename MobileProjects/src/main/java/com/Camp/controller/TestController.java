8a87504ac5411a4f9e4538bfa06f20315cbaf8aa
Dongnyoung <leedo7182@naver.com>
2025-11-12 12:27:21 +0900

    feat: 웹소켓 설정 및 브로드캐스트 구축

---
+++ MobileProjects/src/main/java/com/Camp/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.Camp.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class TestController {
+    @GetMapping("/hello") public String hello(){ return "Hello from Spring"; }
+}

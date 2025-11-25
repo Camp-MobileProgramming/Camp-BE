cf317e71991c7f5cdf621b0f1ad610e9cf8022f2
Dongnyoung <leedo7182@naver.com>
2025-11-25 22:28:36 +0900

    feat: 친구관계레포 생성

---
+++ MobileProjects/src/main/java/com/Camp/domain/friendship/FriendShipRepository.java
@@ -0,0 +1,15 @@
+package com.Camp.domain.friendship;
+
+import com.Camp.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface FriendShipRepository extends JpaRepository<FriendShip, Long> {
+
+    // 내 친구 목록
+    List<FriendShip> findByOwner(User owner);
+
+    // 이미 친구인지 체크
+    boolean existsByOwnerAndFriend(User owner, User friend);
+}

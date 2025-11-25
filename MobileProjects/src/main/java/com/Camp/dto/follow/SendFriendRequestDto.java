555fd325de131e098878963ea44886decfd61545
Dongnyoung <leedo7182@naver.com>
2025-11-25 22:27:13 +0900

    feat: Send,Pending,FriendResponse DTO 구현

---
+++ MobileProjects/src/main/java/com/Camp/dto/follow/SendFriendRequestDto.java
@@ -0,0 +1,10 @@
+package com.Camp.dto.follow;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class SendFriendRequestDto {
+    private String receiverNickname;
+}

8f27c5c9f48dc9a8e6f152fe835959d352af86ae
Dongnyoung <leedo7182@naver.com>
2025-11-28 14:34:15 +0900

    feat: 채팅목록 및 채팅 컨트롤러 등록

--- MobileProjects/src/main/java/com/Camp/domain/chat/ChatMessageRepository.java
+++ MobileProjects/src/main/java/com/Camp/domain/chat/ChatMessageRepository.java
@@ -6,8 +6,10 @@ import com.Camp.domain.chat.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
 
     List<ChatMessage> findByRoomOrderBySentAtAsc(ChatRoom room);
+    Optional<ChatMessage> findTopByRoomOrderBySentAtDesc(ChatRoom room);
 }
